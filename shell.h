f92389f21c8377fe78b07a347fa2356fa7b977fa
ZBLUX <aminekamili@hotmail.com>
2023-08-23 07:49:31 -0700

    small fix

--- shell.h
+++ shell.h
@@ -1,23 +1,37 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <fcntl.h>
 
 extern char **environ;
 
 /*Function prototypes*/
-void execute_command(char *command);
-
+void command(char *argv[], char *env[]);
+void execute_command(char *command, char *args[], char *env[]);
+void execute_and_wait(char *args[], char *env[], char *argv[]);
+void tokenize_command(char *command, char *args[]);
+char *search_command(char *command);
+void free_args(char *argv[]);
+void newline_removal(char *str);
 
 int _putchar(char c);
 void _puts(char *str);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
+int _strspn(const char *str1, const char *str2);
+char *_strstr(char *haystack, char *needle);
+char *_strdup(const char *src);
+char *_strcat(char *dest, const char *src);
 
 #endif
