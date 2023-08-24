4a8e939baf35848c1a5b5f2d6cb98a5cfdaf1686
369hamza <el.ouardi.hamza369@gmail.com>
2023-08-23 17:03:21 -0700

    third fix

---
+++ shell.h
@@ -0,0 +1,47 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <fcntl.h>
+
+extern char **environ;
+
+
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+/*Function prototypes*/
+void command(char *argv[], char *env[]);
+void execute_command(char *command, char *args[], char *env[]);
+void execute_and_wait(char *args[], char *env[], char *argv[]);
+void tokenize_command(char *command, char *args[]);
+char *search_command(char *command);
+void free_args(char *argv[]);
+void newline_removal(char *str);
+int _print(char *string);
+int _print_error(char *string);
+char *check_and_strdup(char *string);
+char *construct_full_path(char *path, char *command);
+
+int _putchar(char c);
+void _puts(char *str);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+int _strspn(const char *str1, const char *str2);
+char *_strstr(char *haystack, char *needle);
+char *_strdup(const char *src);
+char *_strcat(char *dest, const char *src);
+
+#endif
